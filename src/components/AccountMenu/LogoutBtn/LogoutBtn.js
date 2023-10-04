14f566ba9049419d3ca43d1ae582a6a549d58720
MarineEllin <marine.ellin@gmail.com>
2023-10-04 10:28:41 +0200

    fix locale missing in LogOutBtn

--- src/components/AccountMenu/LogoutBtn/LogoutBtn.js
+++ src/components/AccountMenu/LogoutBtn/LogoutBtn.js
@@ -4,14 +4,20 @@ import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./LogoutBtn.module.scss";
 import useCurrentDictionary from "@/hooks/useCurrentDictionary";
+import useCurrentLanguage from "@/hooks/useCurrentLanguage";
+import { useRouter } from "next/navigation";
 
 const LogoutBtn = () => {
-  const dictionary = useCurrentDictionary();
+  const router = useRouter();
+  const locale = useCurrentLanguage();
+  const dictionary = useCurrentDictionary(locale);
+
+  const handleLogout = () => {
+    signOut();
+    router.push(`/${locale}`);
+  };
   return (
-    <div
-      className={styles.logoutBtn}
-      onClick={() => signOut({ callbackUrl: "/" })}
-    >
+    <div className={styles.logoutBtn} onClick={handleLogout}>
       <FontAwesomeIcon icon={faPowerOff} />
       <span>{dictionary && dictionary.log.logout}</span>
     </div>
