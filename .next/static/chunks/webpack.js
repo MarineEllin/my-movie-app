4e7c2fe73608e6778f1979dd8c72d7af213482ae
MarineEllin <marine.ellin@gmail.com>
2023-10-26 11:19:36 +0200

    change localhost

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "60dda123d66dbe0d"; }
+/******/ 		__webpack_require__.h = function() { return "bf4034790836da4e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
